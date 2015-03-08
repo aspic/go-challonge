5421a2426b71e5c455b9c80b5c551317e72098af
Kjetil Mehl <aspic@mehl.no>
2015-03-08 01:03:59 +0100

    unmarshals tournaments

--- challonge.go
+++ challonge.go
@@ -4,9 +4,10 @@ import (
     "log"
     "fmt"
     "net/http"
-    "io/ioutil"
     "encoding/json"
+    "io/ioutil"
     "net/url"
+    "strconv"
 )
 
 const (
@@ -15,6 +16,9 @@ const (
 )
 
 var c ChallongeClient
+
+// Avoids infinite recursion
+type tournament Tournament
 
 type ChallongeClient struct {
     baseUrl string
@@ -25,16 +29,23 @@ type ChallongeClient struct {
 
 type APIResponse struct {
     Tournament Tournament `json:"tournament"`
+
     Errors []string `json:"errors"`
 }
 
 type Tournament struct {
     Name string `json:"name"`
-    Id string `json:"id"`
+    Id int `json:"id"`
     Url string `json:"url"`
     FullUrl string `json:"full_challonge_url"`
+    Participants []Participant `json:"participants"`
 
     client *ChallongeClient
+}
+
+type Participant struct {
+    Id int `json:"id"`
+    Name string `json:"name"`
 }
 
 func (c *ChallongeClient) Print() {
@@ -67,13 +78,17 @@ func (c *ChallongeClient) CreateTournament(name string, subUrl string, open bool
 }
 
 /** returns tournament with the specified id */
-func (c *ChallongeClient) GetTournament(id string) *Tournament {
+func (c *ChallongeClient) GetTournament(id string) (*Tournament, error) {
     v := url.Values{}
     v.Add("include_participants", "1")
     url := c.buildUrl("tournaments/" + id, v)
     response := &APIResponse{}
     c.doGet(url, response)
-    return response.Tournament.withClient(c)
+    log.Print("resp ", response)
+    if len(response.Errors) > 0 {
+        return nil, fmt.Errorf("unable to retrieve tournament: %q", response.Errors[0])
+    }
+    return response.Tournament.withClient(c), nil
 }
 
 /** adds participant to tournament */
@@ -88,11 +103,24 @@ func (t *Tournament) Add(name string) *APIResponse {
 }
 
 /** removes participant from tournament */
-func (t *Tournament) Remove(id string) *APIResponse {
-    url := t.client.buildUrl("tournaments/" + t.Url + "/participants/" + id, nil)
+func (t *Tournament) Remove(name string) error {
+    url := t.client.buildUrl("tournaments/" + t.Url + "/participants/" + strconv.Itoa(t.GetParticipantId(name)), nil)
     response := &APIResponse{}
     c.doDelete(url, response)
-    return response
+    if len(response.Errors) > 0 {
+        return fmt.Errorf("unable to delete participant: %q", response.Errors[0])
+    }
+    return nil
+}
+
+func (t *Tournament) GetParticipantId(name string) int {
+    log.Print("Has: ", t.Participants[0])
+    for _,p := range t.Participants {
+        if p.Name == name {
+            return p.Id
+        }
+    }
+    return 0
 }
 
 func (t *Tournament) withClient(c *ChallongeClient) *Tournament {
@@ -120,6 +148,7 @@ func (c *ChallongeClient) doPost(url string, v interface{}) {
 
 func (c *ChallongeClient) doDelete(url string, v interface{}) {
     req, err := http.NewRequest("DELETE", url, nil)
+    log.Print("deletes resource on url ", url)
     if err != nil {
         log.Fatal("unable to create delete request")
     }
@@ -131,23 +160,36 @@ func (c *ChallongeClient) doDelete(url string, v interface{}) {
 }
 
 func handleResponse(r *http.Response, v interface{}) {
-    defer r.Body.Close()
     body, err := ioutil.ReadAll(r.Body)
+
+    log.Print("got response ", string(body))
+
     if err != nil {
-        log.Fatal("unable to parse response", err)
+        log.Fatal("unable to read response", err)
     }
-    bodyString := string(body)
-    log.Print("Got response: ", bodyString)
     json.Unmarshal(body, v)
+}
+
+func (t *Tournament) UnmarshalJSON(b []byte) (err error) {
+    placeholder := tournament{}
+    if err = json.Unmarshal(b, &placeholder); err == nil {
+        *t = Tournament(placeholder)
+        return
+    }
+    return
 }
 
 func main() {
     c := &ChallongeClient{user: "viking1", version: version, key: "k0PG6IxBQhH8tkpTDlxaUKHLMHRfMy1oycloZgTW"}
     c.Print()
-    tournament, err := c.CreateTournament("foobar3", "foobar3", true, "oo")
+    //tournament, err := c.CreateTournament("foobar4", "foobar4", true, "oo")
+    tournament, err := c.GetTournament("foobar4")
     if err != nil {
         log.Fatal("Got error: ", err)
     } else {
-        tournament.Add("foo")
+        err = tournament.Remove("foo")
+        if err != nil {
+            log.Fatal("error when deleting ", err)
+        }
     }
 }
